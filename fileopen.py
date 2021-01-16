4deccf6f1cba26edbe6769047155ab2b500e7ddd
sr8e <sr@mell0w-5phere.net>
2021-01-17 08:42:22 +0900

    Modify line spacing

--- fileopen.py
+++ fileopen.py
@@ -11,9 +11,10 @@ class FileOpenFrame(ttk.Frame):
         super().__init__(master, **kw)
 
         self.ctype = tk.IntVar()  # conversion type: 0 for cim->png, 1 for png->cim
-        frame_0 = ttk.LabelFrame(self, text='変換タイプ', borderwidth=1, relief=tk.SOLID)
 
+        frame_0 = ttk.LabelFrame(self, text='変換タイプ', borderwidth=1, relief=tk.SOLID)
         frame_0.pack(side=tk.TOP, fill=tk.X, expand=1, pady=15)
+
         radio_c0 = ttk.Radiobutton(frame_0, text='.cim -> .png', value=0, variable=self.ctype)
         radio_c1 = ttk.Radiobutton(frame_0, text='.png -> .cim', value=1, variable=self.ctype)
 
@@ -21,22 +22,21 @@ class FileOpenFrame(ttk.Frame):
         radio_c1.pack(side=tk.LEFT)
 
         self.stype = tk.IntVar()  # file selection type: 0 for folder, 1 for file(s)
+
         frame_1 = ttk.LabelFrame(self, text='変換元ファイル・フォルダ選択', borderwidth=1, relief=tk.SOLID)
         frame_in_1 = ttk.Frame(frame_1)
         frame_in_2 = ttk.Frame(frame_1)
 
         frame_1.pack(side=tk.TOP, fill=tk.X, expand=1, pady=15)
-        frame_in_1.pack(side=tk.TOP, fill=tk.X, expand=1, padx=15, pady=5)
         frame_in_2.pack(side=tk.TOP, fill=tk.X, expand=1, padx=15, pady=5)
+        frame_in_1.pack(side=tk.TOP, fill=tk.X, expand=1, padx=15, pady=5)
 
-        radio_s0 = ttk.Radiobutton(frame_in_1, text='フォルダ選択', value=1, variable=self.stype)
-        radio_s1 = ttk.Radiobutton(frame_in_2, text='ファイル選択', value=0, variable=self.stype)
-
-        radio_s0.pack(side=tk.LEFT)
-        radio_s1.pack(side=tk.LEFT)
+        ttk.Radiobutton(frame_in_1, text='フォルダ選択', value=1, variable=self.stype).pack(side=tk.LEFT)
+        ttk.Radiobutton(frame_in_2, text='ファイル選択', value=0, variable=self.stype).pack(side=tk.LEFT)
 
         self.srcfolderpath = tk.StringVar()
         self.srcfilepath = tk.StringVar()
+
         ttk.Entry(frame_in_1, textvariable=self.srcfolderpath).pack(side=tk.LEFT, fill=tk.X, expand=1)
         ttk.Entry(frame_in_2, textvariable=self.srcfilepath).pack(side=tk.LEFT, fill=tk.X, expand=1)
 
